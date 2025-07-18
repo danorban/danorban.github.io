8506d08976e009fb07bcaca5b74a173b9ee431d9
Dan Orban <dtorban@umn.edu>
2025-07-18 11:22:37 -0500

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
-/*
-    Title: Tech Portfolio Template
-    Version: 1.0
-    Last Change: 03/11/17
+/*!
+    Title: Dev Portfolio Template
+    Version: 1.2.1
+    Last Change: 08/27/2017
+    Author: Ryan Fitzgerald
+    Repo: https://github.com/RyanFitzgerald/devportfolio-template
+    Issues: https://github.com/RyanFitzgerald/devportfolio-template/issues
 
     Description: This file contains all the scripts associated with the single-page
     portfolio website.
@@ -9,8 +12,15 @@
 
 (function($) {
 
+    // Remove no-js class
+    $('html').removeClass('no-js');
+
     // Animate to section when nav is clicked
     $('header a').click(function(e) {
+
+        // Treat as normal link if no-scroll class
+        if ($(this).hasClass('no-scroll')) return;
+
         e.preventDefault();
         var heading = $(this).attr('href');
         var scrollDistance = $(heading).offset().top;
@@ -18,6 +28,11 @@
         $('html, body').animate({
             scrollTop: scrollDistance + 'px'
         }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
+
+        // Hide the menu once clicked if mobile
+        if ($('header').hasClass('active')) {
+            $('header, body').removeClass('active');
+        }
     });
 
     // Scroll to top
@@ -36,7 +51,7 @@
     });
 
     // Create timeline
-    $('#experience-timeline').each(function() {
+    $('.experience-timeline').each(function() {
 
         $this = $(this); // Store reference to this
         $userContent = $this.children('div'); // user content
@@ -61,7 +76,22 @@
 
     });
 
-    // Form Validation
-    // TODO
+    // Open mobile menu
+    $('#mobile-menu-open').click(function() {
+        $('header, body').addClass('active');
+    });
+
+    // Close mobile menu
+    $('#mobile-menu-close').click(function() {
+        $('header, body').removeClass('active');
+    });
+
+    // Load additional projects
+    $('#view-more-projects').click(function(e){
+        e.preventDefault();
+        $(this).fadeOut(300, function() {
+            $('#more-projects').fadeIn(300);
+        });
+    });
 
 })(jQuery);
